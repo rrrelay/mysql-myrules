7c6cdf99d172d1bd533e63711b57bc07c43cd0cf
Mike Chevett <mchevett@appnexus.com>
2014-03-11 23:10:02 -0400

    start using a constructor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var mysql = require('mysql');
 var q = require('q');
+var _ = require('_');
 
 var CONNECTION_INFO = {
 	host     : 'localhost',
@@ -8,29 +9,36 @@ var CONNECTION_INFO = {
 	password : ''
 };
 
-module.exports = function(procName, paramsArray){
-	paramsArray = paramsArray || [];
-	var d = q.defer();
-	var dbConnection = mysql.createConnection(CONNECTION_INFO);
-	var paramsStr = paramsArray
-		.map(function(v){ return "'" + v + "'";})
-		.join(', ');
-	var commandStr = util.format('call banana.%s(%s);', procName, paramsStr);
 
-	dbConnection.connect();
-	dbConnection.query(commandStr, function(err, rows, fields){
-		if (err){
-			global.logger.error('database error running ' + procName);
-			global.logger.error(err);
-			d.reject(err);
-		}
+function MySqlMyRules(connectionInfo){
+	connectionInfo = _.extend({}, connectionInfo, CONNECTION_INFO);
 
-		d.resolve({
-			rows: rows, 
-			fields: fields
+	return function(procName, paramsArray){
+		paramsArray = paramsArray || [];
+		var d = q.defer();
+		var dbConnection = mysql.createConnection(connectionInfo);
+		var paramsStr = paramsArray
+			.map(function(v){ return "'" + v + "'";})
+			.join(', ');
+		var commandStr = util.format('call banana.%s(%s);', procName, paramsStr);
+
+		dbConnection.connect();
+		dbConnection.query(commandStr, function(err, rows, fields){
+			if (err){
+				global.logger.error('database error running ' + procName);
+				global.logger.error(err);
+				d.reject(err);
+			}
+
+			d.resolve({
+				rows: rows, 
+				fields: fields
+			});
 		});
-	});
-	dbConnection.end();
+		dbConnection.end();
 
-	return d.promise;
-};
+		return d.promise;
+	};
+}
+
+module.exports = MySqlMyRules;
